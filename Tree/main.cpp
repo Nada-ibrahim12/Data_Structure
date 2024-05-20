5196b07f9e4b2a69e3640711f4a167833a50d84b
Nada Ibrahim <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-05-20 06:09:55 +0300

    Update main.cpp

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,4 +1,6 @@
 #include "BST.h"
+#include "Heap.h"
+
 using namespace std;
 
 int main() {
@@ -35,6 +37,32 @@ int main() {
     cout << "In-order traversal after removing " << removeKey << ": ";
     bst.inOrderCall();
     cout << endl;
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    Heap<int> heap;
+
+    // Insert elements into the max heap
+    int initial_values[] = {30, 10, 20, 7, 1, 5};
+    for (int val: initial_values) {
+        heap.insert_max(val);
+    }
+
+     cout << "Initial Max-Heap: ";
+    heap.display();
+
+    // Insert an additional element into the max heap
+    heap.insert_max(25);
+     cout << "After inserting 25: ";
+    heap.display();
+
+    // Extract the maximum element from the max heap
+    int maxElement = heap.heap_extract_max();
+     cout << "Extracted Max Element: " << maxElement <<  endl;
+     cout << "After extracting max: ";
+    heap.display();
 
     return 0;
 }
